81b1b751ce235195147ed7b074bfb3c2dd868f84
fruitea <aetiurf@gmail.com>
2024-01-31 17:45:38 +0800

    feat: complete shared_memory_buffer implementation

--- device_buffer/src/lib.rs
+++ device_buffer/src/lib.rs
@@ -1,13 +1,11 @@
 use std::time::Duration;
-mod shared_memory_buffer;
-
-pub use shared_memory_buffer::SharedMemoryBuffer;
 
 pub enum BufferPrivilege {
     BufferHost,
     BufferGuest,
 }
 
+#[derive(Copy, Clone)]
 pub enum IssuingMode {
     SyncIssuing,
     AsyncIssuing,
@@ -17,6 +15,7 @@ pub enum IssuingMode {
 pub enum DeviceBufferError {
     // Define error types, for example:
     IoError(std::io::Error),
+    InvalidOperation,
     Timeout,
     // Add other relevant errors
 }
@@ -25,15 +24,16 @@ const BUFFER_TIMEOUT: Duration = Duration::from_secs(50);
 
 // Trait for the DeviceBuffer: an io abstraction for heterogenous device
 pub trait DeviceBuffer {
-    fn new(privilege: BufferPrivilege) -> Self
-    where
-        Self: Sized;
+    fn put_bytes(&mut self, src: &[u8], mode: Option<IssuingMode>) -> Result<usize, DeviceBufferError>;
 
-    fn put_bytes(&mut self, src: &[u8], mode: Option<IssuingMode>) -> Result<(), DeviceBufferError>;
-
-    fn get_bytes(&mut self, dst: &mut [u8], mode: Option<IssuingMode>) -> Result<(), DeviceBufferError>;
+    fn get_bytes(&mut self, dst: &mut [u8], mode: Option<IssuingMode>) -> Result<usize, DeviceBufferError>;
 
     fn flush_out(&mut self, mode: Option<IssuingMode>) -> Result<(), DeviceBufferError>;
 
     fn fill_in(&mut self, mode: Option<IssuingMode>) -> Result<(), DeviceBufferError>;
 }
+
+
+pub mod shared_memory_buffer;
+
+pub use shared_memory_buffer::SharedMemoryBuffer;
