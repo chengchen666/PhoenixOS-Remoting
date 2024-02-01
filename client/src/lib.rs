bbaee1e7d7f53cbaabe94941c765a9f5e54a873d
fruitea <aetiurf@gmail.com>
2024-02-01 18:46:17 +0800

    chore: client lazy static buffer

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,2 +1,17 @@
+#[macro_use]
+extern crate lazy_static;
+
+extern crate device_buffer;
+use device_buffer::*;
+
 pub mod cuda_runtime;
 pub use cuda_runtime::*;
+
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref BUF_SENDER: Mutex<SharedMemoryBuffer> = {
+        let buf = Mutex::new(SharedMemoryBuffer::new(BufferPrivilege::BufferHost, "/shm_buf", 1024).unwrap());
+        buf
+    };
+}
