0a49abd1e94deec58676dd4716be776212f79e0d
fruitea <aetiurf@gmail.com>
2024-02-01 16:23:36 +0800

    chore: minor

--- client/src/cuda_runtime.rs
+++ client/src/cuda_runtime.rs
@@ -115,3 +115,18 @@ pub enum cudaError {
 }
 
 pub use self::cudaError as cudaError_t;
+
+#[no_mangle]
+pub extern "C" fn cudaGetDevice(device: *mut ::std::os::raw::c_int) -> cudaError_t {
+    unsafe {
+        *device = 0;
+    }
+    println!("cudaGetDevice");
+    return cudaError_t::cudaSuccess;
+}
+
+#[no_mangle]
+pub extern "C" fn cudaSetDevice(device: ::std::os::raw::c_int) -> cudaError_t {
+    println!("cudaSetDevice: {}", device);
+    return cudaError_t::cudaSuccess;
+}
--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,17 +1,2 @@
 pub mod cuda_runtime;
-pub use cuda_runtime::cudaError_t;
-
-#[no_mangle]
-pub extern "C" fn cudaGetDevice(device: *mut ::std::os::raw::c_int) -> cudaError_t {
-    unsafe {
-        *device = 0;
-    }
-    println!("cudaGetDevice");
-    return cudaError_t::cudaSuccess;
-}
-
-#[no_mangle]
-pub extern "C" fn cudaSetDevice(device: ::std::os::raw::c_int) -> cudaError_t {
-    println!("cudaSetDevice: {}", device);
-    return cudaError_t::cudaSuccess;
-}
+pub use cuda_runtime::*;
