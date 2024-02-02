94380eba608e233758d0911007026a7abec162dd
fruitea <aetiurf@gmail.com>
2024-02-03 03:31:28 +0800

    test: test enum to integer casting

--- server/src/cuda_lib/cudart.rs
+++ server/src/cuda_lib/cudart.rs
@@ -132,3 +132,19 @@ extern "C" {
 extern "C" {
     pub fn cudaGetDeviceCount(count: *mut ::std::os::raw::c_int) -> cudaError_t;
 }
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use num::FromPrimitive;
+
+    #[test]
+    fn test_num_derive() {
+        let x: u32 = cudaError_t::cudaSuccess as u32;
+        assert_eq!(x, 0);
+        match cudaError_t::from_u32(1) {
+            Some(v) => assert_eq!(v, cudaError_t::cudaErrorInvalidValue),
+            None => panic!("failed to convert from u32"),
+        }
+    }
+}
