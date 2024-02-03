572149bca2f19e8c8e627a087845eb131bbfe5c8
fruitea <aetiurf@gmail.com>
2024-02-03 14:00:14 +0800

    refactor: minor

--- server/src/cuda_lib/mod.rs
+++ server/src/cuda_lib/mod.rs
@@ -1,3 +1,4 @@
 pub mod cudart;
 
+use super::*;
 pub use cudart::*;
--- server/src/dispatcher/cudart_exe.rs
+++ server/src/dispatcher/cudart_exe.rs
@@ -2,44 +2,6 @@
 use super::*;
 use num::FromPrimitive;
 
-// TODO: implement (de-)serialization for basic types
-
-fn serialize_i32(value: &i32, buffer: &SharedMemoryBuffer) -> Result<usize, DeviceBufferError> {
-    let buf = value.to_ne_bytes();
-    buffer.put_bytes(&buf, None)
-}
-
-fn deserialize_i32(
-    value: &mut i32,
-    buffer: &SharedMemoryBuffer,
-) -> Result<usize, DeviceBufferError> {
-    let mut buf = [0u8; std::mem::size_of::<i32>()];
-    let len_read = buffer.get_bytes(&mut buf, None)?;
-    *value = i32::from_ne_bytes(buf);
-    Ok(len_read)
-}
-
-fn serialize_cudaError_t(
-    value: &cudaError_t,
-    buffer: &SharedMemoryBuffer,
-) -> Result<usize, DeviceBufferError> {
-    let buf = (*value as u32).to_ne_bytes();
-    buffer.put_bytes(&buf, None)
-}
-
-fn deserialize_cudaError_t(
-    value: &mut cudaError_t,
-    buffer: &SharedMemoryBuffer,
-) -> Result<usize, DeviceBufferError> {
-    let mut buf = [0u8; std::mem::size_of::<cudaError_t>()];
-    let len_read = buffer.get_bytes(&mut buf, None)?;
-    match cudaError_t::from_u32(u32::from_ne_bytes(buf)) {
-        Some(v) => *value = v,
-        None => return Err(DeviceBufferError::IoError),
-    }
-    Ok(len_read)
-}
-
 pub fn cudaGetDeviceExe(buffer_sender: &SharedMemoryBuffer, _buffer_receiver: &SharedMemoryBuffer) {
     println!("[{}:{}] cudaGetDevice", std::file!(), function!());
     let mut device: i32 = 0;
