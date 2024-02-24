ad9d1d5cad62af40b38b02e784f14f1fed44cb14
fruitea <aetiurf@gmail.com>
2024-02-24 23:46:12 +0800

    feat: cudart_exe, unify the initial value of params

--- server/src/dispatcher/cudart_exe.rs
+++ server/src/dispatcher/cudart_exe.rs
@@ -3,7 +3,7 @@ use super::*;
 
 pub fn cudaGetDeviceExe<T: CommChannel>(channel_sender: &mut T, _channel_receiver: &mut T) {
     info!("[{}:{}] cudaGetDevice", std::file!(), std::line!());
-    let mut param1: ::std::os::raw::c_int = 0;
+    let mut param1: ::std::os::raw::c_int = Default::default();
     let result = unsafe { cudaGetDevice(&mut param1) };
     channel_sender.send_var(&param1).unwrap();
     channel_sender.send_var(&result).unwrap();
@@ -12,7 +12,7 @@ pub fn cudaGetDeviceExe<T: CommChannel>(channel_sender: &mut T, _channel_receive
 
 pub fn cudaSetDeviceExe<T: CommChannel>(channel_sender: &mut T, channel_receiver: &mut T) {
     info!("[{}:{}] cudaSetDevice", std::file!(), std::line!());
-    let mut param1: ::std::os::raw::c_int = 0;
+    let mut param1: ::std::os::raw::c_int = Default::default();
     channel_receiver.recv_var(&mut param1).unwrap();
     let result = unsafe { cudaSetDevice(param1) };
     channel_sender.send_var(&result).unwrap();
@@ -21,7 +21,7 @@ pub fn cudaSetDeviceExe<T: CommChannel>(channel_sender: &mut T, channel_receiver
 
 pub fn cudaGetDeviceCountExe<T: CommChannel>(channel_sender: &mut T, _channel_receiver: &mut T) {
     info!("[{}:{}] cudaGetDeviceCount", std::file!(), std::line!());
-    let mut param1: ::std::os::raw::c_int = 0;
+    let mut param1: ::std::os::raw::c_int = Default::default();
     let result = unsafe { cudaGetDeviceCount(&mut param1) };
     channel_sender.send_var(&param1).unwrap();
     channel_sender.send_var(&result).unwrap();
