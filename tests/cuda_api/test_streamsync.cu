212830ceb8eff2af29b7bf97c815355fd43013a9
fruitea <aetiurf@gmail.com>
2024-03-09 19:17:39 +0800

    test: cudaStreamSynchronize

---
+++ tests/cuda_api/test_streamsync.cu
@@ -0,0 +1,15 @@
+#include <chrono>
+#include <cuda_runtime.h>
+#include <iostream>
+
+int main()
+{
+    const int iterations = 1;
+    int count = 0;
+    cudaError_t error;
+
+    error = cudaStreamSynchronize(0);
+    std::cout << "cudaStreamSynchronize(0) returned " << error << std::endl;
+
+    return 0;
+}
