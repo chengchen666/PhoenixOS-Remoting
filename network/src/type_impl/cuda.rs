cbeb6f9fb61fe83fa6f86a8ec1bb46c61be0bfbe
fruitea <aetiurf@gmail.com>
2024-03-12 22:20:47 +0800

    test: CUresult unittest

--- network/src/type_impl/cuda.rs
+++ network/src/type_impl/cuda.rs
@@ -100,3 +100,24 @@ pub enum cudaError_enum {
 pub use self::cudaError_enum as CUresult;
 
 pub type CUmodule = usize;
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+    use crate::FromPrimitive;
+    use crate::ringbufferchannel::{
+        channel::META_AREA,
+        LocalChannelBufferManager, RingBuffer
+    };
+
+    #[test]
+    fn test_CUresult_io() {
+        let mut buffer: RingBuffer<LocalChannelBufferManager> =
+            RingBuffer::new(LocalChannelBufferManager::new(10 + META_AREA));
+        let a = CUresult::CUDA_ERROR_ALREADY_ACQUIRED;
+        let mut b = CUresult::CUDA_SUCCESS;
+        a.send(&mut buffer).unwrap();
+        b.recv(&mut buffer).unwrap();
+        assert_eq!(a, b);
+    }
+}
