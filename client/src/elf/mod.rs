0b2fe5db4774e0a7f384d5d97aeb4c626dccbf1f
fruitea <aetiurf@gmail.com>
2024-03-12 22:05:21 +0800

    fix: elf interface should be instance's methods

--- client/src/elf/mod.rs
+++ client/src/elf/mod.rs
@@ -1,5 +1,5 @@
 pub mod interfaces;
-use self::interfaces::{fat_header, kernel_info_t, elf2_init, kernel_infos_free};
+use self::interfaces::{elf2_init, fat_header, kernel_info_t, kernel_infos_free};
 
 pub struct ElfController;
 
@@ -13,13 +13,18 @@ impl ElfController {
     }
 
     // wrapper of interfaces
-    pub fn add_kernel_host_func(func: *const ::std::os::raw::c_void, info: *mut kernel_info_t) {
+    pub fn add_kernel_host_func(
+        &self,
+        func: *const ::std::os::raw::c_void,
+        info: *mut kernel_info_t,
+    ) {
         unsafe { interfaces::add_kernel_host_func(func, info) };
     }
-    pub fn add_kernel_name(name: *const ::std::os::raw::c_char, info: *mut kernel_info_t) {
+    pub fn add_kernel_name(&self, name: *const ::std::os::raw::c_char, info: *mut kernel_info_t) {
         unsafe { interfaces::add_kernel_name(name, info) };
     }
     pub fn elf2_get_fatbin_info(
+        &self,
         fatbin: *const fat_header,
         fatbin_mem: *mut *mut u8,
         fatbin_size: *mut usize,
@@ -27,26 +32,31 @@ impl ElfController {
         unsafe { interfaces::elf2_get_fatbin_info(fatbin, fatbin_mem, fatbin_size) }
     }
     pub fn elf2_parameter_info(
+        &self,
         memory: *mut ::std::os::raw::c_void,
         memsize: usize,
     ) -> ::std::os::raw::c_int {
         unsafe { interfaces::elf2_parameter_info(memory, memsize) }
     }
     pub fn elf2_symbol_address(
+        &self,
         symbol: *const ::std::os::raw::c_char,
     ) -> *mut ::std::os::raw::c_void {
         unsafe { interfaces::elf2_symbol_address(symbol) }
     }
-    pub fn find_kernel_host_func(func: *const ::std::os::raw::c_void) -> *mut kernel_info_t {
+    pub fn find_kernel_host_func(&self, func: *const ::std::os::raw::c_void) -> *mut kernel_info_t {
         unsafe { interfaces::find_kernel_host_func(func) }
     }
-    pub fn find_kernel_name(name: *const ::std::os::raw::c_char) -> *mut kernel_info_t {
+    pub fn find_kernel_name(&self, name: *const ::std::os::raw::c_char) -> *mut kernel_info_t {
         unsafe { interfaces::find_kernel_name(name) }
     }
-    pub fn utils_parameter_info(path: *mut ::std::os::raw::c_char) -> ::std::os::raw::c_int {
+    pub fn utils_parameter_info(&self, path: *mut ::std::os::raw::c_char) -> ::std::os::raw::c_int {
         unsafe { interfaces::utils_parameter_info(path) }
     }
-    pub fn utils_search_info(kernelname: *const ::std::os::raw::c_char) -> *mut kernel_info_t {
+    pub fn utils_search_info(
+        &self,
+        kernelname: *const ::std::os::raw::c_char,
+    ) -> *mut kernel_info_t {
         unsafe { interfaces::utils_search_info(kernelname) }
     }
 }
