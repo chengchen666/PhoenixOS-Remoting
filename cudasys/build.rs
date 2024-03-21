42d5eff61be959aa3fc11fd650952b0cc1aab2de
fruitea <aetiurf@gmail.com>
2024-03-21 08:51:49 +0800

    chore: porting

--- cudasys/build.rs
+++ cudasys/build.rs
@@ -1,18 +1,82 @@
-use std::env;
-use std::path::PathBuf;
+extern crate glob;
+use glob::glob;
+use std::{env, path::PathBuf};
 
-fn main() {
+pub fn read_env() -> Vec<PathBuf> {
+    if let Ok(path) = env::var("CUDA_LIBRARY_PATH") {
+        let split_char = ":";
+        path.split(split_char).map(|s| PathBuf::from(s)).collect()
+    } else {
+        vec![]
+    }
+}
+
+// output (candidates, valid_path)
+pub fn find_cuda() -> (Vec<PathBuf>, Vec<PathBuf>) {
+    let mut candidates = read_env();
+    candidates.push(PathBuf::from("/opt/cuda"));
+    candidates.push(PathBuf::from("/usr/local/cuda"));
+    for e in glob("/usr/local/cuda-*").unwrap() {
+        if let Ok(path) = e {
+            candidates.push(path)
+        }
+    }
+
+    let mut valid_paths = vec![];
+    for base in &candidates {
+        let lib = PathBuf::from(base).join("lib64");
+        if lib.is_dir() {
+            valid_paths.push(lib.clone());
+            valid_paths.push(lib.join("stubs"));
+        }
+        let base = base.join("targets/x86_64-linux");
+        let header = base.join("include/cuda.h");
+        if header.is_file() {
+            valid_paths.push(base.join("lib"));
+            valid_paths.push(base.join("lib/stubs"));
+            continue;
+        }
+    }
+    eprintln!("Found CUDA paths: {:?}", valid_paths);
+    (candidates, valid_paths)
+}
+
+fn bind_gen(
+    paths: Vec<PathBuf>,
+    library: &str,
+    allowlist_types: Vec<&str>,
+    allowlist_vars: Vec<&str>,
+    allowlist_funcs: Vec<&str>,
+) {
+    // find the library header path
+    let mut header_path = None;
+    for path in paths {
+        let header = path.join(format!("include/{}.h", library));
+        if header.is_file() {
+            header_path = Some(header);
+            break;
+        }
+    }
+    let header_path = header_path.expect("Could not find CUDA header file");
+
     // The bindgen::Builder is the main entry point to bindgen, and lets you build up options for
     // the resulting bindings.
-    let bindings = bindgen::Builder::default()
+    let mut bindings = bindgen::Builder::default()
         // The input header we would like to generate bindings for.
-        .header("/usr/local/cuda-11.3/include/cuda_runtime.h")
-        // Whitelist types, functions, and variables
-        .allowlist_type("^cuda.*")
-        .allowlist_type("^surfaceReference")
-        .allowlist_type("^textureReference")
-        .allowlist_var("^cuda.*")
-        .allowlist_function("^cuda.*")
+        .header(header_path.to_str().unwrap());
+
+    // Whitelist types, functions, and variables
+    for ty in allowlist_types {
+        bindings = bindings.allowlist_type(ty);
+    }
+    for var in allowlist_vars {
+        bindings = bindings.allowlist_var(var);
+    }
+    for func in allowlist_funcs {
+        bindings = bindings.allowlist_function(func);
+    }
+
+    let bindings = bindings
         // Set the default enum style to be more Rust-like
         .default_enum_style(bindgen::EnumVariation::Rust {
             non_exhaustive: false,
@@ -32,10 +96,34 @@ fn main() {
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $CARGO_MANIFEST_DIR/src/cuda_runtime.rs.
+    // Write the bindings to the `$CARGO_MANIFEST_DIR/src/library.rs`.
     let root = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
     let out_dir = root.join("src/bindings");
     bindings
-        .write_to_file(out_dir.join("cuda_runtime.rs"))
+        .write_to_file(out_dir.join(format!("{}.rs", library)))
         .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    let (cuda_paths, cuda_libs) = find_cuda();
+
+    // Tell rustc to link the CUDA library.
+    for path in &cuda_libs {
+        println!("cargo:rustc-link-search=native={}", path.display());
+    }
+
+    println!("cargo:rustc-link-lib=dylib=cuda");
+    println!("cargo:rustc-link-lib=dylib=cudart");
+    println!("cargo:rustc-link-lib=dylib=nvidia-ml");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=CUDA_LIBRARY_PATH");
+
+    // Use bindgen to automatically generate the FFI (in `src/bindings`).
+    bind_gen(
+        cuda_paths,
+        "cuda_runtime",
+        vec!["^cuda.*", "^surfaceReference", "^textureReference"],
+        vec!["^cuda.*"],
+        vec!["^cuda.*"],
+    );
 }
