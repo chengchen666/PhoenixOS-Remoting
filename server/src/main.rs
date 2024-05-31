d96f71bdc7c4dc18a189caf5db26c89f34fcce95
liuyang <green_egg@sjtu.edu.cn>
2024-05-31 13:59:05 +0000

    chcore: update RDMA config, remove unused code

--- server/src/main.rs
+++ server/src/main.rs
@@ -10,9 +10,6 @@ fn main() {
     if env::var("RUST_LOG").is_err() {
         env::set_var("RUST_LOG", "debug");
     }
-    if std::env::var("EMU_TYPE").is_err() {
-        std::env::set_var("EMU_TYPE", "server");
-    }
     env_logger::init();
     launch_server();
 }
