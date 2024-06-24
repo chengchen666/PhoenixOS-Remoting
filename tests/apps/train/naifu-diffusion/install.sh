7bfb809e7bb3b4fbbec86e1b4e7ed84b56617d82
fruitea <aetiurf@gmail.com>
2024-06-25 03:26:56 +0800

    feat: naifu-diffusion pip, walkby pytorch version check

--- tests/apps/train/naifu-diffusion/install.sh
+++ tests/apps/train/naifu-diffusion/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i "1s/.*/__version__ = '1.13.0'/" /opt/conda/lib/python3.8/site-packages/torch/version.py
+
 pip install -r requirements.txt
 
 wget -O animesfw.tgz https://pub-2fdef7a2969f43289c42ac5ae3412fd4.r2.dev/animesfw.tgz
